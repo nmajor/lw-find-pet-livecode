70c464ace3cc21ac4bdcb17af8d8d54ae59412aa
Nicholas Major <nick@nmajor.com>
2018-02-20 18:37:42 +0000

    Added EEEEEEVERYTHING

---
+++ db/seeds.rb
@@ -0,0 +1,4 @@
+Pet.create(name: 'Rufus', phone: '555-555-5555')
+Pet.create(name: 'Fifel', phone: '555-555-5556')
+Pet.create(name: 'Spot', phone: '555-555-5557')
+Pet.create(name: 'Arrow', phone: '555-555-5558')
